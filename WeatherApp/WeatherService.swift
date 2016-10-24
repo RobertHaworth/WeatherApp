3e573365b70492a7ef0f576106358d5fa6c7a53e
robertchaworth <robert@robertchaworth.com>
2016-10-23 22:20:18 -0500

    Added Alamofire via Cocoapods. Started WeatherService class to handle interaction with the Weather API of choice.

---
+++ WeatherApp/WeatherService.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherService.swift
+//  WeatherApp
+//
+//  Created by Robert Haworth on 10/23/16.
+//  Copyright © 2016 Robert Haworth. All rights reserved.
+//
+
+import UIKit
+
+class WeatherService {
+    
+    let serverURL = "api.openweathermap.org/data/2.5/weather"
+    
+    // API routes
+    
+    //?zip=94040
+    
+    
+    func getWeather(zip:String) {
+        
+    }
+
+}
